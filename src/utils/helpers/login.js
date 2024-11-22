97d0241d601d107bf141de3e73623ae71f32b736
nhatviet <daonhatviet.dev@gmail.com>
2024-11-22 23:02:43 +0700

    fix

--- src/utils/helpers/login.js
+++ src/utils/helpers/login.js
@@ -1,6 +1,6 @@
 export function checkAuth() {
     var dataSession = JSON.parse(sessionStorage.getItem('Auth')) || null;
-    if (dataSession === null) {
-        window.location.href = '/Login';
-    }
+    // if (dataSession === null) {
+    //     window.location.href = '/Login';
+    // }
 }
